0e4a472c59ba4a4206aa07183104383069ee96d0
Alexis Aguilar <alexmerona@gmail.com>
2023-04-25 17:01:51 -0400

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
-import { NavBar } from "@/components/main/NavBar";
-import { Banner } from "@/components/main/Banner";
-import { About } from "@/components/main/About";
-import { Skills } from "@/components/main/Skills";
-import { Projects } from "@/components/main/Projects";
-import { Footer } from "@/components/main/Footer";
+import { NavBar } from "@/components/index/NavBar";
+import { Banner } from "@/components/index/Banner";
+import { About } from "@/components/index/About";
+import { Skills } from "@/components/index/Skills";
+import { Projects } from "@/components/index/Projects";
+import { Footer } from "@/components/index/Footer";
 import Head from "next/head";
 
 function Home() {
