759f93afb7bc51f01157ba476317716d2b17c45c
Alexis <alexmerona@gmail.com>
2023-04-25 16:46:23 -0400

    head tags for each route

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,17 +4,26 @@ import { About } from "@/components/main/About";
 import { Skills } from "@/components/main/Skills";
 import { Projects } from "@/components/main/Projects";
 import { Footer } from "@/components/main/Footer";
+import Head from "next/head";
 
 function Home() {
   return (
-    <div className="Home">
-      <NavBar />
-      <Banner />
-      <Projects />
-      <About />
-      <Skills />
-      <Footer />
-    </div>
+    <>
+      <Head>
+        <title>alexisintech | Full Stack Software Engineer</  title>
+        <meta
+        name="description"
+        content="Alexis Aguilar is an insatiably inquisitive  software engineer who is passionate about full stack web   development. Take a minute to learn a little about her  and see a few of her projects."/>
+      </Head>
+      <div className="Home">
+        <NavBar />
+        <Banner />
+        <Projects />
+        <About />
+        <Skills />
+        <Footer />
+      </div>
+    </>
   );
 }
 
--- src/pages/links.tsx
+++ src/pages/links.tsx
@@ -1,16 +1,26 @@
 import Hero from "@/components/links/Hero";
 import Links from "@/components/links/Links";
 import Footer from "@/components/links/Footer";
+import Head from "next/head";
 
 function LinksIndex() {
   return (
-    <div className="links-index min-h-screen bg-black text-white">
-      <div className="container mx-auto flex min-h-screen flex-col items-center text-center">
-        <Hero />
-        <Links />
-        <Footer />
+    <>
+      <Head>
+        <title>alexisintech | Full Stack Software Engineer</title>
+        <meta
+          name="description"
+          content="Alexis Aguilar's social links, all in one place for convenience."
+        />  
+      </Head>
+      <div className="links-index min-h-screen bg-black text-white">
+        <div className="container mx-auto flex min-h-screen flex-col items-center text-center">
+          <Hero />
+          <Links />
+          <Footer />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
